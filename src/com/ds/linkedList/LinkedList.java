6243991af92f226f8faa7e35fb063b6a3f149b3b
Shark1906 <amanraj1906@gmail.com>
2021-11-18 20:29:45 +0530

    ADDED : LinkedList_uc3

--- src/com/ds/linkedList/LinkedList.java
+++ src/com/ds/linkedList/LinkedList.java
@@ -36,6 +36,25 @@ public class LinkedList {
 		node.next = head;
 		head = node;
 	}
+	
+	public boolean append(int data) {
+		boolean isAdded = false;
+		
+		Node node = new Node(data);
+		if(head == null) {
+			head = node;
+			isAdded = true;
+		}
+		else {
+			Node temp = head;
+			while(temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = node;
+			isAdded = true;
+		}
+		return isAdded;
+	}
 		
 	public void print() {
 		Node temp = head;
